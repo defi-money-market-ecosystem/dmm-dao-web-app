9793d156831d3a403083a4631a3574c8fbba9691
Corey Caplan <coreycaplan3@gmail.com>
2020-09-26 14:44:16 -0400

    Updated yieldfarming.js hook

--- src/contexts/YieldFarming.js
+++ src/contexts/YieldFarming.js
@@ -1,6 +1,6 @@
-import { useInterval } from '../hooks'
+import { useWeb3React } from '../hooks'
 import { DMM_API_URL } from '../utils'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const YIELD_FARMING_ROUTER_ADDRESS = '0x8209eD0259F99Abd593E8cd26e6a14f224C6cccA'
 export const YIELD_FARMING_PROXY_ADDRESS = '0x502e90e092Cd08e6630e8E1cE426fC6d8ADb3975'
@@ -8,18 +8,26 @@ export const YIELD_FARMING_PROXY_ADDRESS = '0x502e90e092Cd08e6630e8E1cE426fC6d8A
 export function useIsYieldFarmingActive() {
   const [isFarmingActive, setIsFarmingActive] = useState(false)
 
-  useInterval(() => {
+  const { library } = useWeb3React()
+
+  const fetchIsFarmingActive = useCallback(() => {
     fetch(`${DMM_API_URL}/v1/yield-farming/is-active`)
       .then(response => response.json())
-      .then(response => {
-        // isFarmingActive.current = response?.data
-        setIsFarmingActive(!isFarmingActive)
-      })
+      .then(response => setIsFarmingActive(response?.data))
       .catch(error => {
         console.log('error ', error)
         setIsFarmingActive(false)
       })
-  }, 15 * 1000, true)
+  }, [])
+
+  useEffect(() => {
+    fetchIsFarmingActive()
+    library.on('block', fetchIsFarmingActive)
+
+    return () => {
+      library.removeListener('block', fetchIsFarmingActive)
+    }
+  }, [fetchIsFarmingActive, library])
 
   return isFarmingActive
 }
